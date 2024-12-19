075fe02baa9d1cd0e265887cf74824ed314db02d
thejorabek <boyxorozovjorabek@gmail.com>
2024-12-19 21:39:35 +0500

    login

---
+++ ToDoList/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  ToDoList
+//
+//  Created by Jorabek Boyxorozov on 19/12/24.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
