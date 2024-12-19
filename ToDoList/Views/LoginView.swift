dcfb3a455b5fe2512a6b5e4f9620a318ebb89f5d
thejorabek <boyxorozovjorabek@gmail.com>
2024-12-19 21:40:10 +0500

    login

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HeaderView()
+            Form{
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Email Address", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button{
+                    
+                }label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        Text("Login")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                }
+            }
+            VStack{
+                Text("New around here?")
+                Button("Create an account"){
+                    
+                }
+            }
+            .padding(.bottom, 50)
+            Spacer()
+            
+        }
     }
 }
-
 #Preview {
     LoginView()
 }
