485c00876791f2d9e2c6d2b3821f69a6c9f8381e
thejorabek <boyxorozovjorabek@gmail.com>
2024-12-20 19:02:06 +0500

    login & register

--- ToDoList/ViewModels/LoginViewViewModel.swift
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class LoginViewViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var password: String = ""
+    
+    init(){}
+    
+    func login(){
+        
+    }
+    
+    func validate(){
+    
+    }
+    
+}
--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -9,18 +9,17 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State var email: String = ""
-    @State var password: String = ""
+    @StateObject var viewModel = LoginViewViewModel()
 
     var body: some View {
         NavigationView{
             VStack{
                 HeaderView(title: "To Do List", subtitle: "Get things done", angle: 15, backgroundColor: .pink)
                 Form{
-                    TextField("Email ", text: $email)
+                    TextField("Email ", text: $viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
                         .autocapitalization(.none)
-                    SecureField("Password ", text: $password)
+                    SecureField("Password ", text: $viewModel.password)
                         .textFieldStyle(DefaultTextFieldStyle())
                     TLButton(title: "Login", backgroundColor: .blue){
                         
